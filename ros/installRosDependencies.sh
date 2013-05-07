9b4011fc975e0838c189bef052fbcc0942e47870
Brandon Alexander <baalexander@gmail.com>
2013-05-06 23:16:20 -0700

    Initial commit of ROSCon 2013 examples.

---
+++ ros/installRosDependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt-get install ros-groovy-ros-base \
+    ros-groovy-navigation \
+    ros-groovy-robot-pose-publisher \
+    ros-groovy-rosbridge-server \
+    ros-groovy-tf2-web-republisher \
+    ros-groovy-rosapi \
+    ros-groovy-robot-state-publisher \
+    ros-groovy-robot-model-visualization \
+    ros-groovy-wg-common \
+    ros-groovy-turtlebot
